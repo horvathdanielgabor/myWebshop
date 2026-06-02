2f61ce67d035f11c536d48ff5f7aca7ab7db3ecb
horvathdanielgabor <horvathdanielgabor@turr.hu>
2026-06-02 13:14:43 +0200

    Login window

--- myWebshop/App.xaml.cs
+++ myWebshop/App.xaml.cs
@@ -9,6 +9,9 @@ namespace myWebshop
     /// </summary>
     public partial class App : Application
     {
+        static string database = "eKreta.db";
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(path, database);
     }
 
 }
